47a4482989ebcf4469b794538692cc628c556745
said_sadu <sadullaev.1996@gmail.com>
2022-08-17 12:55:55 +0300

    решена 1 задача

---
+++ ex001/Program.cs
@@ -0,0 +1,21 @@
+﻿int[] arr = new int[10];
+int k = 0;
+Random rnd = new Random();
+
+Console.WriteLine("Рандомный массив");
+for (int i = 0; i < 10; i++)
+{
+    arr[i] = rnd.Next(100, 1000);
+    Console.Write("{0} ", arr[i]);
+}
+
+foreach (int element in arr)
+{
+    if (element % 2 == 0)
+    {
+        k++;
+
+    }
+}
+Console.WriteLine();
+Console.WriteLine("Количество четных элементов {0}", k);
